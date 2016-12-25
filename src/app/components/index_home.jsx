3cd1f1559c31cc2595aaf0008f90e3d96e63e241
Onurbon <marnette.bruno@gmail.com>
2016-12-25 20:04:07 +0100

    start design with background video of Paros

--- src/app/components/index_home.jsx
+++ src/app/components/index_home.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
+const videoUrl = "https://www.youtube.com/embed/WwSYT-4KwpI?controls=0&showinfo=0&rel=0&autoplay=1&loop=1"
+
 export default () => {
-  return <div> Home Page of our application! </div>;
+  return <div>
+    <div className='our-names'> Bruno & Despo </div>
+    <div className='wed-date'> June 10, 2016 </div>
+    <div className='wed-place'> Paros, Greece </div>
+
+    <div className="video-background">
+      <div className="video-blur"/>
+      <div className="video-foreground">
+        <iframe src={videoUrl} frameborder="0" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>;
 };
