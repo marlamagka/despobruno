93a4ec209731c061c7fde1faad52638faf366a2b
Onurbon <marnette.bruno@gmail.com>
2017-01-01 18:13:21 -0300

    detect and persist language

--- src/app/reducers/copy_reducer.js
+++ src/app/reducers/copy_reducer.js
@@ -12,9 +12,23 @@ function copyInLang(lang) {
   return res
 }
 
-export default function (state = copyInLang('EN'), action) {
+var navLang = window.navigator.userLanguage || window.navigator.language
+if (navLang.startsWith('en')) {
+  navLang = 'EN'
+}
+if (navLang === 'el') {
+  navLang = 'GR'
+}
+if (navLang === 'fr') {
+  navLang = 'FR'
+}
+
+var defaultLang = localStorage._lang || navLang
+
+export default function (state = copyInLang(defaultLang), action) {
   switch (action.type) {
     case SWITCH_LANG:
+      localStorage._lang = action.payload
       return copyInLang(action.payload);
     default:
       return state;
