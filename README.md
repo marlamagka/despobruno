4ec3691816c33fac5ced592f983e65d0e25f2d23
Onurbon <marnette.bruno@gmail.com>
2017-02-19 15:57:34 +0000

    fix images deployment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # despobruno
+
+Develop:
+```
+npm i
+npm run dev
+```
+
+Deploy:
+```
+npm run build
+cp -r images dist/images
+firebase deploy
+```
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -8,28 +8,30 @@ class Home extends Component {
 
   componentDidMount() {
     /* global window */
-    const YT = window.YT;
-    new YT.Player('muteYouTubeVideoPlayer', {
-      videoId: 'WwSYT-4KwpI',   // YouTube Video ID
-      width: 560,               // Player width (in px)
-      height: 316,              // Player height (in px)
-      playerVars: {
-        autoplay: 1,            // Auto-play the video on load
-        controls: 0,            // Show pause/play buttons in player
-        showinfo: 0,            // Hide the video title
-        modestbranding: 1,      // Hide the Youtube Logo
-        loop: 1,                // Run the video in a loop
-        fs: 0,                  // Hide the full screen button
-        cc_load_policy: 0,      // Hide closed captions
-        iv_load_policy: 3,      // Hide the Video Annotations
-        autohide: 0,            // Hide video controls when playing
-      },
-      events: {
-        onReady(e) {
-          e.target.mute();
+    setTimeout(() => {
+      const YT = window.YT;
+      new YT.Player('muteYouTubeVideoPlayer', {
+        videoId: 'WwSYT-4KwpI',   // YouTube Video ID
+        width: 560,               // Player width (in px)
+        height: 316,              // Player height (in px)
+        playerVars: {
+          autoplay: 1,            // Auto-play the video on load
+          controls: 0,            // Show pause/play buttons in player
+          showinfo: 0,            // Hide the video title
+          modestbranding: 1,      // Hide the Youtube Logo
+          loop: 1,                // Run the video in a loop
+          fs: 0,                  // Hide the full screen button
+          cc_load_policy: 0,      // Hide closed captions
+          iv_load_policy: 3,      // Hide the Video Annotations
+          autohide: 0,            // Hide video controls when playing
         },
-      },
-    });
+        events: {
+          onReady(e) {
+            e.target.mute();
+          },
+        },
+      });
+    }, 200);
   }
 
   render() {
